11f216afbb6aa6e4ad978948a79dd6ff8e8a7c1d
Seyi Longe <oluwaseun.longe@nih.gov>
2020-08-04 18:10:18 -0400

    (#43) Analytics: Other Search Results Click
    Closes #43
    
    * Added tracking event and click handler
    * Added integration tests
    * Updated unit tests
    * Updated title link in integration test
    * Updates for tracking parameter based on review
    * Updated `resultsIndex` computation
    * Removed type cast

--- src/components/molecules/search-results-list/__tests__/result-list-item.test.js
+++ src/components/molecules/search-results-list/__tests__/result-list-item.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import ResultsListItem from '../results-list-item';
+import MockAnalyticsProvider from '../../../../tracking/mock-analytics-provider';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('<ResultsListItem /> component', () => {
+	const analyticsHandler = jest.fn(() => {});
+
 	afterEach(cleanup);
 
 	test('renders the search item in component when object supplied', () => {
@@ -14,7 +19,11 @@ describe('<ResultsListItem /> component', () => {
 			description:
 				'Desmoid tumors grow from the connective tissue in your body.',
 		};
-		const { container } = render(<ResultsListItem result={result} />);
+		const { container } = render(
+			<MockAnalyticsProvider analyticsHandler={analyticsHandler}>
+				<ResultsListItem result={result} resultIndex={1} />
+			</MockAnalyticsProvider>
+		);
 		expect(screen.getByText(/Desmoid Tumor/)).toBeInTheDocument();
 		expect(screen.getByText(/Desmoid tumors grow/)).toBeInTheDocument();
 		expect(container.querySelector('.result__list-item')).toBeInTheDocument();
@@ -22,5 +31,23 @@ describe('<ResultsListItem /> component', () => {
 		expect(container.querySelector('.result__link')).toBeInTheDocument();
 		expect(container.querySelector('.result__description')).toBeInTheDocument();
 		expect(container.querySelector('.result__url')).toBeInTheDocument();
+	});
+
+	test('should fire tracking event when search result item title link is clicked', () => {
+		const result = {
+			title: 'Desmoid Tumor - National Cancer Institute',
+			url: 'https://www.cancer.gov/pediatric/tumors/soft-tissue/desmoid',
+			contentType: 'cgvInfographic',
+			description:
+				'Desmoid tumors grow from the connective tissue in your body.',
+		};
+		const { container } = render(
+			<MockAnalyticsProvider analyticsHandler={analyticsHandler}>
+				<ResultsListItem result={result} resultIndex={1} />
+			</MockAnalyticsProvider>
+		);
+		const searchTitleLink = container.querySelector('.result__link');
+		fireEvent.click(searchTitleLink);
+		expect(analyticsHandler).toHaveBeenCalledTimes(1);
 	});
 });
