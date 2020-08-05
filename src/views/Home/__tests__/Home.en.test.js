d6a1efcaa2250ac273f71e72bf7e32a7ba90267b
kate-mashkina <mashkinaea@nci-02181235-ml.myfiosgateway.com>
2020-08-05 13:41:09 -0400

    (#39)  BestBets link click analytics - added analytics handler in the bestbets component - unit tests for analytics in home and best bets components - integration tests - changed branch coverage to 75 (to get onto react)

--- src/views/Home/__tests__/Home.en.test.js
+++ src/views/Home/__tests__/Home.en.test.js
@@ -10,6 +10,8 @@ import { i18n } from '../../../utils';
 
 jest.mock('../../../store/store.js');
 
+const analyticsHandler = jest.fn(() => { });
+
 describe('Home component(English)', () => {
 	test('should show no results found page for "achoo" as search keyword', async () => {
 		const basePath = '/';
@@ -64,4 +66,82 @@ describe('Home component(English)', () => {
 			},
 		]);
 	});
+
+
+	test('should fire analytics on page load', async () => {
+		const basePath = '/';
+		const canonicalHost = 'https://www.example.gov';
+		const language = 'en';
+		const title = 'NCI Search Results';
+
+		useStateValue.mockReturnValue([
+			{
+				appId: 'mockAppId',
+				basePath,
+				canonicalHost,
+				language,
+				title,
+			},
+		]);
+
+		const client = {
+			query: async () => ({
+				error: false,
+				status: 200,
+				payload: {
+					meta: {
+						totalResults: 5,
+						from: 0,
+					},
+					results: [
+						{
+							"title": "Desmoid Tumor - National Cancer Institute",
+							"url": "https://www.cancer.gov/pediatric-adult-rare-tumor/rare-tumors/rare-soft-tissue-tumors/desmoid-tumor",
+							"contentType": "cgvInfographic",
+							"description": "Desmoid tumors grow from the connective tissue in your body. Desmoid tumors are benign, which means they are not cancer, but they are very difficult to get rid of and can be painful to live with. Learn more about diagnosis, treatments, and prognosis for desmoid tumors."
+						},
+						{
+							"title": "Pancreatic Neuroendocrine Tumors (Islet Cell Tumors) Treatment (PDQ®)–Patient Version - National Cancer Institute",
+							"url": "https://www.cancer.gov/types/pancreatic/patient/pnet-treatment-pdq",
+							"contentType": "pdqCancerInfoSummary",
+							"description": "Pancreatic neuroendocrine tumors (islet cell tumors) treatments include surgery, hormone therapy, chemotherapy, targeted therapy, and supportive care. Learn more about the treatment of newly diagnosed and recurrent pancreatic neuroendocrine tumors in this expert-reviewed summary."
+						},
+						{
+							"title": "Wilms Tumor and Other Childhood Kidney Tumors Treatment (PDQ®)–Health Professional Version - National Cancer Institute",
+							"url": "https://www.cancer.gov/types/kidney/hp/wilms-treatment-pdq",
+							"contentType": "pdqCancerInfoSummary",
+							"description": "Treatment options for Wilms tumor and other childhood kidney tumors include surgery (nephrectomy), chemotherapy, radiation, and kidney transplantation. Get detailed information about the treatment for newly diagnosed and recurrent Wilms and other kidney tumors in this summary for clinicians."
+						},
+						{
+							"title": "",
+							"url": "https://www.cancer.gov/types/pancreatic/hp/pnet-treatment-pdq",
+							"contentType": "pdqCancerInfoSummary",
+							"description": ""
+						},
+						{
+							"title": "Brain Tumors—Health Professional Version - National Cancer Institute",
+							"url": "https://www.cancer.gov/types/brain/hp",
+							"contentType": "cgvVideo",
+							"description": "Brain and spinal cord tumors include anaplastic astrocytomas and glioblastomas, meningiomas, pituitary tumors, schwannomas, ependymomas, and sarcomas. Find evidence-based information on brain tumor treatment, research, genetics, and statistics."
+						}
+					],
+					links: null,
+				},
+			}),
+		};
+
+		await act(async () => {
+			render(
+				<MockAnalyticsProvider analyticsHandler={analyticsHandler}>
+					<ClientContextProvider client={client}>
+						<MemoryRouter initialEntries={['/?swKeyword=tumor']}>
+							<Home />
+						</MemoryRouter>
+					</ClientContextProvider>
+				</MockAnalyticsProvider>
+			);
+		});
+		expect(analyticsHandler).toHaveBeenCalledTimes(1);
+	});
+
 });
