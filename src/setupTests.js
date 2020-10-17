fb2a7a1e5cba601aad367262382bdc28ea97e08a
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2020-10-17 16:15:46 -0400

    (#110) move fixtures function to setupTests, preventing tests folder from being included in build

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import path from 'path';
+
+const basePath = path.join(__dirname, '..', 'support', 'mock-data');
+
+const fixtures = {
+	getFixture: (filePath) => {
+		return require(`${basePath}${filePath}`);
+	},
+};
+
+global.getFixture = fixtures.getFixture;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as fixtures } from './fixtures';
 export { i18n } from './i18n';
 export { getProductTestBase } from './getProductTestBase';
 export { formatNumberToThousands } from './number';
@@ -7,4 +6,4 @@ export { getKeyValueFromObject } from './objects';
 export { TokenParser } from './replaceTokens';
 export { emboldenSubstring, splitSentencesToArray } from './strings';
 export { getKeyValueFromQueryString } from './url';
-export {default as closest} from './closest';
+export { default as closest } from './closest';
